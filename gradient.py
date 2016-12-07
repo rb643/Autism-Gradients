0a39212a80d80c45214faf7ff1f67aca4e013b5b
rb643 <rb643@cam.ac.uk>
2016-12-07 13:24:24 +0000

    Update gradient.py

--- gradient.py
+++ gradient.py
@@ -19,12 +19,13 @@ Created on Mon Dec 01 15:05:56 2016
 # main/Embs/ > for diffusion embedding files
 
 # download ABIDE data:
-# http://preprocessed-connectomes-project.org/abide/download.html 
+# http://preprocessed-connectomes-project.org/abide/download.html
 # python download_abide_preproc.py -d rois_cc400 -p cpac -s filt_noglobal -o data/ -x 'M' -gt 18 -lt 55
 
 ## lets start with some actual script
 # import useful things
 import numpy as np
+import os
 import nibabel as nib
 from sklearn.metrics import pairwise_distances
 
@@ -52,6 +53,8 @@ for i in onlyfiles:
 # select the ones that don't have missing data
 ids = np.where([len(i) == 0 for i in missingarray])[0]
 selected = [onlyfiles[i] for i in ids]
+# could be useful to have one without pathnames later one
+selected2 = [os.path.basename(onlyfiles[i]) for i in ids]
 print(len(selected))
 
 # run the diffusion embedding
@@ -74,23 +77,23 @@ for i in selected:
 
     for ii in range(dcon.shape[0]):
         #print "Row %d" % ii
-        dcon[ii, dcon[ii,:] < perc[ii]] = 0 
- 
+        dcon[ii, dcon[ii,:] < perc[ii]] = 0
+
     # If there are any left then set them to zero
     dcon[dcon < 0] = 0
 
     # compute the pairwise correctionlation distances
     aff = 1 - pairwise_distances(dcon, metric = 'cosine')
-    
+
     # start saving
     savename = os.path.basename(filename)
-    np.save("Affn/"+savename+"_cosine_affinity.npy", aff)
+    np.save("./Affn/"+savename+"_cosine_affinity.npy", aff)
     # get the diffusion maps
     emb, res = embed.compute_diffusion_map(aff, alpha = 0.5)
     # Save results
-    np.save("Embs/"+savename+"_embedding_dense_emb.npy", emb)
-    np.save("Embs/"+savename+"_embedding_dense_res.npy", res)
-    np.save("Embs/"+savename+"_embedding_dense_res_veconly.npy", res['vectors']) #store vectors only
+    np.save("./Embs/"+savename+"_embedding_dense_emb.npy", emb)
+    np.save("./Embs/"+savename+"_embedding_dense_res.npy", res)
+    np.save("./Embs/"+savename+"_embedding_dense_res_veconly.npy", res['vectors']) #store vectors only
 
 # get dimension
 #resolution = len(res['vectors'][0,:])
@@ -103,11 +106,11 @@ from pySTATIS import statis
 
 #load vectors
 names = list(xrange(392))
-X = [np.load("Embs/"+ os.path.basename(filename)+"_embedding_dense_res_veconly.npy") for filename in selected] 
+X = [np.load("./Embs/"+ os.path.basename(filename)+"_embedding_dense_emb.npy") for filename in selected2]
 out = statis.statis(X, names, fname='statis_results.npy')
+statis.project_back(X, out['Q'], path = "./Regs/",fnames = selected2)
+np.save("Mean_Vec.npy",out['F'])
 
-
-### THIS IS STILL WORK IN PROGRESS...
 # saving everything in one dump
 import pickle
 with open('output.pickle' ,'w') as f:
@@ -115,7 +118,47 @@ with open('output.pickle' ,'w') as f:
 #with open('output.pickle') as f:  # Python 3: open(..., 'rb')
 #   selected, out = pickle.load(f)
 
-# plot to surface for inspection (DOESN'T WORK... :( yet..)
-from cluster_roi import make_image_from_bin_renum
-binfile = out['F'][:,1]
-make_image_from_bin_renum(image,binfile,mask)
+######################### plotting #######################
+# plot to surface for inspection
+# this cell in only necessary for plotting below
+import matplotlib.pylab as plt
+import nilearn
+import nilearn.plotting
+
+import numpy as np
+import nibabel as nib
+
+def rebuild_nii(num):
+
+    data = np.load('Mean_Vec.npy')
+    a = data[:,num].copy()
+    nim = nib.load('cc400_roi_atlas.nii')
+    imdat=nim.get_data()
+    imdat_new = imdat.copy()
+
+    for n, i in enumerate(np.unique(imdat)):
+        if i != 0:
+            imdat_new[imdat == i] = a[n-1] * 100000 # scaling factor. Could also try to get float values in nifti...
+
+    nim_out = nib.Nifti1Image(imdat_new, nim.get_affine(), nim.get_header())
+    nim_out.set_data_dtype('float32')
+    # to save:
+    #nim_out.to_filename('res.nii')
+
+    nilearn.plotting.plot_epi(nim_out)
+    return(nim_out)
+
+for i in range(10):
+    nims = rebuild_nii(i)
+
+######################### excel #######################
+import pandas as pd
+# read in csv
+df_phen = pd.read_csv('Phenotypic_V1_0b_preprocessed1.csv')
+# add a column that matches the filename
+for i in df_phen:
+    df_phen['filename'] = join(df_phen['FILE_ID']+"_rois_cc400.1D")
+
+df_phen['selec'] = np.where(df_phen['filename'].isin((selected2)), 1, 0)
+
+######################### compare slopes #######################
